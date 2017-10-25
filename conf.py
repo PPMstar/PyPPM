b35d9f0438ad6740231f33ca1be3ae57d6cf5137
jerichooconnell <jericho.oconnell@gmail.com>
2017-10-25 21:17:41 +0000

    m

--- conf.py
+++ conf.py
@@ -32,14 +32,10 @@ sys.path.append(os.path.abspath('sphinxext'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'matplotlib.sphinxext.only_directives',
-    'matplotlib.sphinxext.plot_directive',
-    'IPython.sphinxext.ipython_directive',
-    'IPython.sphinxext.ipython_console_highlighting',
     'sphinx.ext.mathjax',
     'sphinx.ext.doctest',
     'sphinx.ext.inheritance_diagram'
-    #'numpydoc'
+    'numpydoc'
 
 ]
 
