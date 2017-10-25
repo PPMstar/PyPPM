72d6abd857015c23d2b79eb251093dd83880284f
jerichooconnell <jericho.oconnell@gmail.com>
2017-10-25 21:55:21 +0000

    m

--- conf.py
+++ conf.py
@@ -34,8 +34,6 @@ extensions = [
     'sphinx.ext.napoleon',
     'matplotlib.sphinxext.only_directives',
     'matplotlib.sphinxext.plot_directive',
-    'IPython.sphinxext.ipython_directive',
-    'IPython.sphinxext.ipython_console_highlighting',
     'sphinx.ext.mathjax',
     'sphinx.ext.doctest',
     'sphinx.ext.inheritance_diagram',
