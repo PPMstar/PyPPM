72c7ef211aafce916d937b2c0dd759c2b5b11f1c
jerichooconnell <jericho.oconnell@gmail.com>
2017-10-27 18:27:35 +0000

    Generated gh-pages for commit 8686263 Author: jerichooconnell <jericho.oconnell@gmail.com>
        building and pushing docs

--- includeme-1.py
+++ includeme-1.py
@@ -1,6 +1,3 @@
 import ppmpy.ppm as ppm
 D1 = ppm.yprofile('/data/ppm_rpod2/YProfiles/O-shell-M25/D1')
-I4 = ppm.yprofile('/data/ppm_rpod2/YProfiles/C-ingestion/I4')
-D1.vprofs( [240,330],fname_type = 'range', initial_conv_boundaries = False, ifig = 3, lw=3.,run='D1')
-I4.vprofs( [270,372],fname_type = 'range', initial_conv_boundaries = False,
-          ifig = 3, lw=1.,run='I4',lims=[3.5,9.2,0,50])
+D1.vprofs([90,100],log_logic=True)
