f74ea1a77cf949020e94a4eca81a1af42cf01ab5
jerichooconnell <jericho.oconnell@gmail.com>
2017-10-27 18:21:52 +0000

    Generated gh-pages for commit b3a75a5 Author: jerichooconnell <jericho.oconnell@gmail.com>
        building and pushing docs

--- includeme-1.py
+++ includeme-1.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.random.randn(1000)
-plt.hist( x, 20)
-plt.grid()
-plt.title("Stats are great!")
+import ppmpy.ppm as ppm
+D1 = ppm.yprofile('/data/ppm_rpod2/YProfiles/O-shell-M25/D1')
+I4 = ppm.yprofile('/data/ppm_rpod2/YProfiles/C-ingestion/I4')
+D1.vprofs( [240,330],fname_type = 'range', initial_conv_boundaries = False, ifig = 3, lw=3.,run='D1')
+I4.vprofs( [270,372],fname_type = 'range', initial_conv_boundaries = False,
+          ifig = 3, lw=1.,run='I4',lims=[3.5,9.2,0,50])
