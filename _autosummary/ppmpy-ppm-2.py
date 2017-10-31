b98d4670582b73c768070025d44a7572ff0b8c77
jerichooconnell <jericho.oconnell@gmail.com>
2017-10-31 16:53:12 +0000

    Generated gh-pages for commit 39050b4 Author: jerichooconnell <jericho.oconnell@gmail.com>
        building and pushing docs

---
+++ _autosummary/ppmpy-ppm-2.py
@@ -0,0 +1,8 @@
+from ppmpy import ppm
+data_dir = '/data/ppm_rpod2/YProfiles/'
+project = 'O-shell-M25'
+ppm.set_YProf_path(data_dir+project)
+
+D2=ppm.yprofile('D2')
+D1=ppm.yprofile('D1')
+prof_compare([D2,D1])
