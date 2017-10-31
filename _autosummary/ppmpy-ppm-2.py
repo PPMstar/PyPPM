6ab6b6776b4903da650ea612cec6cf8163b0dbce
jerichooconnell <jericho.oconnell@gmail.com>
2017-10-31 19:29:00 +0000

    Generated gh-pages for commit 973d091 Author: jerichooconnell <jericho.oconnell@gmail.com>
        building and pushing docs

--- _autosummary/ppmpy-ppm-2.py
+++ _autosummary/ppmpy-ppm-2.py
@@ -4,5 +4,4 @@ project = 'O-shell-M25'
 ppm.set_YProf_path(data_dir+project)
 
 D2=ppm.yprofile('D2')
-D1=ppm.yprofile('D1')
-ppm.prof_compare([D2,D1])
+D2.prof_time([0,5,10],logy=False,num_type='time')
--- _autosummary/ppmpy-ppm-4.py
+++
@@ -1,7 +0,0 @@
-from ppmpy import ppm
-data_dir = '/data/ppm_rpod2/YProfiles/'
-project = 'O-shell-M25'
-ppm.set_YProf_path(data_dir+project)
-
-D2=ppm.yprofile('D2')
-D2.prof_time([0,5,10],logy=False,num_type='time')
