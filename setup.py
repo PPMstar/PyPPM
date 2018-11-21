f487d1d3bc747c37610edeff85597a26908acfac
Falk Herwig <fherwig@uvic.ca>
2018-11-21 02:50:28 -0800

    update setup

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name = "ppmpy",
     version = "1.0",
     packages = find_packages(),
-    install_requires = ["numpy", "setuptools","math","nugridpy","matplotlib","scipy"],
+    install_requires = ["numpy", "setuptools","nugridpy","matplotlib","scipy"],
     author = "Falk Herwig, Sam Jones, Robert Andrassy, Daniel Alexander Bertolino Conti ",
     author_email = "fherwig@uvic.ca")
